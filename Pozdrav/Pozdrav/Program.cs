bc0367b395a8f5dae39b1a52b99e1e050f5f9c3b
Barbara Pušnar <barbara.pusnar@scng.si>
2024-09-27 09:04:31 +0200

    Testni projekt

---
+++ Pozdrav/Pozdrav/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+namespace Pozdrav
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pozdrav = "Kako si kaj?";
+            StreamWriter sw=new StreamWriter("a1.txt");
+            sw.WriteLine(pozdrav);
+            sw.Close();
+            Console.WriteLine(pozdrav);
+        }
+    }
+}
